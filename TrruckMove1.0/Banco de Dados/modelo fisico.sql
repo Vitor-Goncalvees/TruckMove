54b4fd3c78c849a2c94e27dc0081b486263c6c1b
Vitor-Goncalvees <12300128@aluno.cotemig.com.br>
2024-06-24 01:15:18 -0300

    Add files via upload
    Commit Sprint03

---
+++ TrruckMove1.0/Banco de Dados/modelo fisico.sql
@@ -0,0 +1,78 @@
+create database truckmoveinicial;
+use truckmoveinicial;
+
+
+CREATE TABLE Usuario( -- Motorista
+  ID INT primary key NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  senha VARCHAR(45) NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  email VARCHAR(110) NOT NULL,
+  telefone VARCHAR(15) NOT NULL,
+  sexo VARCHAR(15) NOT NULL,
+  data_nasc DATE NOT NULL,
+  cidade VARCHAR(45) NOT NULL,
+  estado VARCHAR(45) NOT NULL,
+  endereco VARCHAR(120) NOT NULL,
+  veiculo VARCHAR(20) NOT NULL,
+  placa_veiculo VARCHAR(7) NOT NULL,
+  carteira_motor VARCHAR(60) NOT NULL
+) engine InnoDB;
+
+
+CREATE TABLE Veiculo(
+id_veiculo int primary key not null auto_increment,
+veiculo varchar(30) not null,
+placa varchar(30) not null,
+ano varchar(100) not null,
+modelo varchar(50) not null,
+id_motorista int not null,
+foreign key (id_motorista) references Usuario(ID)
+)engine InnoDB;
+
+
+insert into usuario values(default,'paulo',1122, 1234, 'paulo@gmail.com', 123456, 'masculino', 11-08-2000, 'belo','minas','santa cruz','caminhao',999999,123456);
+insert veiculo values (default,'caminhao',999999,123456,'ford',1);
+
+
+
+
+create table Cliente(
+ID INT primary key NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  senha VARCHAR(45) NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  email VARCHAR(110) NOT NULL,
+  telefone VARCHAR(15) NOT NULL,
+  sexo VARCHAR(15) NOT NULL,
+  data_nasc DATE NOT NULL,
+  cidade VARCHAR(45) NOT NULL,
+  estado VARCHAR(45) NOT NULL,
+  endereco VARCHAR(120) NOT NULL
+);
+
+
+create table Pedidos(
+id_pedido int auto_increment primary key,
+id_cliente int,
+volume_carga varchar(30),
+distancia double
+);
+
+
+CREATE TABLE Ajudante(
+ajudante_id int auto_increment primary key,
+nome varchar (200),
+cpf char(14),
+email varchar(50),
+telefone varchar(40),
+username VARCHAR(50) NOT NULL,
+password VARCHAR(255) NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+)
+;
+
+select * from cliente;
+select* from pedidos;
+select * from usuario;
+select * from veiculo;
---
+++ TrruckMove1.0/Código/Motorista/Cadastro/cadastroMotorista.php
@@ -0,0 +1,243 @@
+<?php
+
+if(isset($_POST['submit'])){
+
+    /*print_r('Nome: ' . $_POST['nome']);
+    print_r('<br>');
+
+    print_r('CPF: ' . $_POST['cpf']);
+    print_r('<br>');
+
+    print_r('Email: '. $_POST['email']);
+    print_r('<br>');
+
+    print_r('Telefone: '. $_POST['telefone']);
+    print_r('<br>');
+
+    print_r('Sexo: '. $_POST['genero']);
+    print_r('<br>');
+
+    print_r('Data de Nascimento: '. $_POST['datanascimento']); 
+    print_r('<br>');
+
+    print_r('Cidade: '. $_POST['cidade']); 
+    print_r('<br>');
+
+    print_r('Estado: '. $_POST['estado']); 
+    print_r('<br>');
+
+    print_r('Endereço: '. $_POST['endereco']);
+    print_r('<br>');
+
+    print_r('Veículo: '. $_POST['tipo']);
+    print_r('<br>');
+
+    print_r('Placa: '. $_POST['placa']);
+    print_r('<br>');
+    
+    print_r('Carteira de Motorista: '. $_POST['carteiramotor']);
+    */
+
+    include_once('config.php');
+    
+    $nome =  $_POST['nome'];
+    $password = $_POST['senha'];
+    $cpf =  $_POST['cpf'];
+    $email = $_POST['email'];
+    $tel = $_POST['telefone'];
+    $sexo = $_POST['genero'];
+    $data_nasc = $_POST['datanascimento'];
+    $cidade = $_POST['cidade'];
+    $estado =  $_POST['estado'];
+    $endereco = $_POST['endereco'];
+   
+
+    $result = mysqli_query($conexao, "INSERT INTO usuarios(nome,senha,cpf,email,telefone,sexo,data_nasc,cidade,estado,endereco) VALUES ('$nome','$password','$cpf','$email','$tel','$sexo','$data_nasc','$cidade','$estado','$endereco')");
+
+   
+    header('Location: TelaLogin.php');
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro Motorista</title>
+
+<style>
+    body{
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: linear-gradient(to left, #003082,  rgb(20, 147, 220));
+}
+.box{
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: rgba(0, 0, 0, 0.6);
+    padding: 15px;
+    border-radius: 15px;
+    
+  
+}
+fieldset{
+    border: 3px solid dodgerblue;
+}
+
+legend{
+    border: 1px solid dodgerblue;
+    padding: 10px;
+    text-align: center;
+    background-color: dodgerblue;
+    border-radius: 8px;
+    color: white;
+}
+.inputBox{
+    position: relative;
+}
+.inputUser{
+    background: none;
+    border: none;
+    border-bottom: 1px solid white;
+    outline: none;
+    color: white;
+    font-size: 15px;
+   width: 50%;
+    letter-spacing: 2px;
+}
+
+.labelInput{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    pointer-events: none;
+    transition: .5s;
+}
+
+.inputUser:focus ~ .labelInput, 
+.inputUser:valid ~ .labelInput{
+    
+    top: -20px;
+    font-size: 12px;
+    color: dodgerblue;
+}
+
+#datanascimento{
+    border: none;
+    padding: 8px;
+    border-radius: 10px;
+    outline: none;
+}
+
+#submit{
+    background-image: linear-gradient(to right, rgb(0, 92, 197),  rgb(90, 20, 220));
+    border: none;
+    width: 100%;
+    padding: 15px;
+    border-radius: 10px;
+    color: white;
+    font-size: 15px;
+    cursor: pointer;
+}
+
+#submit:hover{
+    background-image: linear-gradient(to right, rgb(0, 80, 172),  rgb(80, 19, 195));
+}
+</style>
+</head>
+<body>
+<a href="home.php">Voltar</a>
+    <div class="box">
+        <form action="cadastroMotorista.php" method="POST">
+            <fieldset>
+                <legend><b>Cadastro de Motorista</b></legend>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="nome" id="nome" class="inputUser" required>
+                    <label for="nome" class="labelInput">Nome Completo</label>
+                   
+                </div>
+                <br>
+                <br>
+                <div class="inputBox">
+                    <input type="password" name="senha" id="senha" class="inputUser" required>
+                    <label for="senha" class="labelInput">Senha</label>
+                   
+                </div>
+                <br>
+                <br>
+                <div class="inputBox">
+                    <input type="number" name="cpf" id="cpf" class="inputUser" required>
+                    <label for="cpf" class="labelInput">CPF</label>
+                   
+                </div>
+                <br>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="email" id="email" class="inputUser" required>
+                    <label for="email" class="labelInput">Email</label>
+                   
+                </div>
+                <br>
+                <br>
+
+                <div class="inputBox">
+                    <input type="tel" name="telefone" id="telefone" class="inputUser" required>
+                    <label for="telefone" class="labelInput">Número de telefone</label>
+                   
+                </div>
+                <br>
+                <br>
+                <p>Sexo:</p>
+                <input type="radio" id="feminino" name="genero" value="feminino" required>
+                <label for="feminino">Feminino</label>
+                <br>
+                <input type="radio" id="masculino" name="genero" value="masculino" required>
+                <label for="masculino">Masculino</label>
+                <br>
+                <input type="radio" id="outro" name="genero" value="outro" required>
+                <label for="outro">Outro</label>
+                <br>
+                <br>
+
+
+                
+
+                
+                    <label for="datanascimento"><b>Data de Nascimento:</b></label>
+                    <input type="date" name="datanascimento" id="datanascimento" required>
+          
+                <br>
+                <br>
+
+                <div class="inputBox">
+                    <input type="text" name="cidade" id="cidade" class="inputUser" required>
+                    <label for="telefone" class="labelInput">Cidade</label>
+                   
+                </div>
+                <br>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="estado" id="estado" class="inputUser" required>
+                    <label for="estado" class="labelInput">Estado</label>
+                   
+                </div>
+                <br>
+                <br>
+                <div class="inputBox">
+                    <input type="text" name="endereco" id="endereco" class="inputUser" required>
+                    <label for="endereco" class="labelInput">Endereço</label>
+                   
+                </div>
+                <br>
+                <br>
+
+                <input type="submit" name="submit" id="submit">
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
